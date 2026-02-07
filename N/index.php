2efbe86f6a96f468ffb75e0f983ee776a26a8fe9
S0upM4n <Fernandosiecm@gmail.com>
2026-02-07 10:24:50 -0300

    Add files via upload

---
+++ N/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1 id="f" onclick="call()">erm actually, this is a php file🤓</h1>
+    <br>
+    <DIV class="cool" onclick="sigman()" style="background-color:black;width:25vw;height:25vw; margin:auto; border: 2px double black">
+    <p >HEY </p>
+
+    </DIV>
+    <br><br><br>
+    <ol type="1" id="idk">
+  <li></li>
+  <li></li>
+  <li></li>
+  <button onclick="add()"> here</button>
+    </ol>  
+    <script>
+
+        
+        function call() {
+            document.getElementById("f").innerHTML =
+                "I HATE <b>CARROT CAKE</b>";
+        }
+
+        function sigman(){
+        document.getElementsByClassName("cool")[0].style.backgroundColor = "red";
+        }
+
+function add() {
+    const list = document.getElementById("idk");
+
+    const newListItem = document.createElement("li");
+    newListItem.innerHTML = "  "; 
+
+    list.appendChild(newListItem);
+}
+
+
+    </script>
+</body>
+</html>
